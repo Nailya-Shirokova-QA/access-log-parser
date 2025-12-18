f602bed26e4a9f1339a3d845ba7eb0f1ec61e0e5
Nailya-Shirokova-QA <naila.shirokova@yandex.ru>
2025-12-18 12:38:16 +0400

    Добавлена программа для вычисления операций

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,37 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main (String [] args) {
-        System.out.println("Введите текст и нажмите <Enter>: " +
-                Math.random());
-        String text=new Scanner(System.in) .nextLine();
-                System.out.println("Длина текста: "+text.length());
+    public static void main(String[] args) {
+        // Создаем сканер для чтения ввода из консоли
+        Scanner scanner = new Scanner(System.in);
+
+        // Получаем первое число
+        System.out.println("Введите первое число:");
+        int firstNumber = scanner.nextInt();
+
+        // Получаем второе число
+        System.out.println("Введите второе число:");
+        int secondNumber = scanner.nextInt();
+
+        // Вычисляем сумму
+        int sum = firstNumber + secondNumber;
+
+        // Вычисляем разность
+        int difference = firstNumber - secondNumber;
+
+        // Вычисляем произведение
+        int product = firstNumber * secondNumber;
+
+        // Вычисляем частное (используем double для точности)
+        double quotient = (double) firstNumber / secondNumber;
+
+        // Выводим результаты
+        System.out.println("Сумма: " + sum);
+        System.out.println("Разность: " + difference);
+        System.out.println("Произведение: " + product);
+        System.out.println("Частное: " + quotient);
+
+        // Закрываем сканер (хорошая практика)
+        scanner.close();
     }
 }
